5f627732c7da925f601622c21c71421946123fbc
mrawshan <54669576+mrawshan@users.noreply.github.com>
2021-01-18 08:48:35 +0300

    I change the javascript file

--- script.js
+++ script.js
@@ -15,30 +15,26 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
-//Function for random color
-function randomColor() {
-    // var randomValue = Math.floor((Math.random() * 256) + 0);
-    return `#${((1 << 24) * Math.random() | 0).toString(16)}`;
-}
-
 //Function to generate random color
-function generateRandomColor() {
-    let colourHex1 = randomColor();
-    let colourHex2 = randomColor();
-
-    body.style.background = `linear-gradient(to right, ${colourHex1}, ${colourHex2}`;
-    css.textContent = `${body.style.background};`;
-
-    color1.value = colourHex1; //set inputs to show the right colours
-    color2.value = colourHex2;
+function randomColor() {
+    let hexColor = Math.random().toString(16);
+    hexColor = "#" + hexColor.slice(2, 8);
+    return hexColor;
 }
+
+//Function to change the background after button click
+function randomBackground() {
+    color1.value = randomColor();
+    color2.value = randomColor();
+    setGradient();
+  }
+
+//Page onload show the default color
+body.onload = setGradient();
 
 //Input color1 and color2
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
-//Page onload show the default color
-body.onload = setGradient();
-
 //Button to generate the random color
-rendomBtn.addEventListener("click", generateRandomColor);
+rendomBtn.addEventListener("click", randomBackground);
